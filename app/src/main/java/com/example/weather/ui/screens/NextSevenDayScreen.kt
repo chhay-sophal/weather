2a7e9ada13f7fb8052050f8d7395c93062b1e3c3
Sophal Chhay <134204301+chhay-sophal@users.noreply.github.com>
2024-08-29 20:48:50 +0700

    Update NextSevenDayScreen.kt

--- app/src/main/java/com/example/weather/ui/screens/NextSevenDayScreen.kt
+++ app/src/main/java/com/example/weather/ui/screens/NextSevenDayScreen.kt
@@ -16,9 +16,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
